e095b41cded9847e62481ae3ec56a438988924e4
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-09-23 11:03:50 +0800

    new project

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
+
+fn main() -> io::Result<()> {
+    println!("Hello, world!");
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app = run(terminal);
+    ratatui::restore();
+    app
+}
+
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                .white()
+                .on_blue();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
